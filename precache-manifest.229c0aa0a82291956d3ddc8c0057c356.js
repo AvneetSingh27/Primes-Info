64a7e73a3ff86784a0abded096278553775bf38f
AvneetSingh27 <prabh.cs18@nitp.ac.in>
2020-05-21 21:27:05 +0530

    Updates

---
+++ precache-manifest.229c0aa0a82291956d3ddc8c0057c356.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5681df5b523e32240950f7b492240cff",
+    "url": "/Primes-Info/index.html"
+  },
+  {
+    "revision": "d1c6e4c35a2300f70f89",
+    "url": "/Primes-Info/static/css/main.df13225d.chunk.css"
+  },
+  {
+    "revision": "e582819a3394ceab2f22",
+    "url": "/Primes-Info/static/js/2.303517bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Primes-Info/static/js/2.303517bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1c6e4c35a2300f70f89",
+    "url": "/Primes-Info/static/js/main.2f51133a.chunk.js"
+  },
+  {
+    "revision": "7a0b9ed455ee39551ef0",
+    "url": "/Primes-Info/static/js/runtime-main.ecf45f8e.js"
+  }
+]);
