704a85cad82fcb860ae0ead21f202116ebb7cde6
AvneetSingh27 <prabh.cs18@nitp.ac.in>
2020-05-21 20:51:47 +0530

    first commit

--- src/components/home/MoviesContainer.js
+++ src/components/home/MoviesContainer.js
@@ -7,12 +7,7 @@ export class MoviesContainer extends Component {
     render() {
         const {movies} = this.props;
         let content = '';
-        // let notFound = (
-        //     <div className="container">
-        //         <p>No results Found!</p>
-        //     </div>
-        // );
-        content = movies.Response === 'True' ? movies.Search.map((movie,index) => <MovieCard key={index} movie = {movie} />) : 'notFound';
+        content = movies.Response === 'True' ? movies.Search.map((movie,index) => <MovieCard key={index} movie = {movie} />) : 'not Found';
         return (
             <div className="row">
                {content} 
